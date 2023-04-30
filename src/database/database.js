e6641af8ff9d9f866e65d9598652729905062bfd
ravaattar <rava.naufal2014@gmail.com>
2023-04-30 22:47:57 +0700

    update: env gitignore

--- src/database/database.js
+++ src/database/database.js
@@ -1,11 +1,12 @@
+require('dotenv').config();
 const mysql = require('mysql2/promise');
 
 const connection = mysql.createPool({
-    host: 'localhost',
-    port: 3306,
-    user: 'root',
-    password: 'Sivaren1',
-    database: 'skyshi',
+    host: process.env.MYSQL_HOST || 'localhost',
+    port: process.env.MYSQL_PORT || 3306,
+    user: process.env.MYSQL_USER || 'root',
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DBNAME,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0,
